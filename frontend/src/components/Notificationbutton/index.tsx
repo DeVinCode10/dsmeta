752de234cb46e9d8eabe10096cf8a47cdca60b73
VENICIOS COSTA <venicios.costa.cef@gmail.com>
2022-11-19 22:55:12 -0300

    First component

---
+++ frontend/src/components/Notificationbutton/index.tsx
@@ -0,0 +1,13 @@
+import iconNot from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={iconNot} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
