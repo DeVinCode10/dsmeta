d19581c8717ed9df23e018fb51aa3a93d9359e22
VENICIOS COSTA <venicios.costa.cef@gmail.com>
2022-11-21 00:13:46 -0300

    send notification. Envia notificação específica sobre um vendedor ao clicar ao clicar no botão correspondente.

--- frontend/src/components/Notificationbutton/index.tsx
+++ frontend/src/components/Notificationbutton/index.tsx
@@ -1,10 +1,27 @@
+import axios from "axios";
 import iconNot from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from "../../utils/request";
 import './styles.css';
 
+type Props = {
+    saleId: number;
 
-function NotificationButton() {
+}
+
+function handleClick(id: number) {
+
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+        .then(Response => {
+            console.log("sucesso");
+        })
+
+}
+
+
+function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        //vai criar o handleClick .
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={iconNot} alt="Notificar" />
         </div>
     )
